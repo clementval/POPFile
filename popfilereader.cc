d545540c81206bfda61a153ee45599ced0ee7d9a
Clement Valentin <clementval@gmail.com>
2014-12-09 16:14:38 +0100

    Fix indent

--- popfilereader.cc
+++ popfilereader.cc
@@ -21,10 +21,9 @@
  */
 POPFileReader::POPFileReader()
 {
-
-	cout << "[POPFILEREADER] Created on " << POPGetHost() << popcendl;	
-	popfilebuffer_ref = new POPFileBuffer();
-   pt_read_locker == NULL;
+  cout << "[POPFILEREADER] Created on " << POPGetHost() << popcendl;	
+  popfilebuffer_ref = new POPFileBuffer();
+  pt_read_locker == NULL;
 }
    
 /**
@@ -32,7 +31,7 @@ POPFileReader::POPFileReader()
  */
 POPFileReader::~POPFileReader()
 {
-	sem_unlink(reader_semname.c_str());
+  sem_unlink(reader_semname.c_str());
 }
 
 /**
@@ -42,16 +41,16 @@ POPFileReader::~POPFileReader()
  */
 void POPFileReader::set_id(int value)
 {
-	identifier = value;
-	if(pt_read_locker == NULL){
-		std::stringstream semname;
-		semname << "/pfrs";
-		semname << identifier;
-		reader_semname = semname.str();
-		sem_unlink(reader_semname.c_str());
-		pt_read_locker = sem_open(reader_semname.c_str(), O_CREAT|O_EXCL, 0600, 1);
-		sem_wait(pt_read_locker);
-	}
+  identifier = value;
+  if(pt_read_locker == NULL){
+    std::stringstream semname;
+    semname << "/pfrs";
+    semname << identifier;
+    reader_semname = semname.str();
+    sem_unlink(reader_semname.c_str());
+    pt_read_locker = sem_open(reader_semname.c_str(), O_CREAT|O_EXCL, 0600, 1);
+    sem_wait(pt_read_locker);
+  }
 }
    
 /**
@@ -62,11 +61,11 @@ void POPFileReader::set_id(int value)
  */
 void POPFileReader::read_in_strip(long start, long offset)
 {	
-	//cout << "[POPFILEREADER] Read in strip " << strip_path.GetString() << " from:" << start << " to:" << start+offset << popcendl;	
-	POPString data = pfm_ref->readFromStrip(strip_path, start, offset);
-	//cout << "[POPFILEREADER] Read from strip (bytes) " << strlen(data.GetString()) << popcendl;	
-	popfilebuffer_ref->add_data(data);
-	sem_post(pt_read_locker);
+  //cout << "[POPFILEREADER] Read in strip " << strip_path.GetString() << " from:" << start << " to:" << start+offset << popcendl;	
+  POPString data = pfm_ref->readFromStrip(strip_path, start, offset);
+  //cout << "[POPFILEREADER] Read from strip (bytes) " << strlen(data.GetString()) << popcendl;	
+  popfilebuffer_ref->add_data(data);
+  sem_post(pt_read_locker);
 }
 
 /**
@@ -76,18 +75,17 @@ void POPFileReader::read_in_strip(long start, long offset)
  */
 POPString POPFileReader::read_current_buffer(long size)
 {
-	
-	while(popfilebuffer_ref->get_size_input_data() < size){
-		cout << "Before lock: id = " << identifier << " data = "<< popfilebuffer_ref->get_size_input_data() << "Size" << size <<  popcendl;
-		sem_wait(pt_read_locker);
-	}	
-	POPString data;
-	if(size != -1)
-		data = popfilebuffer_ref->buffer_get(size).c_str();
-	else 
-		data = popfilebuffer_ref->buffer_get_offset().c_str();
-	cout << "[POPFILEREADER] Sent data size " << strlen(data.GetString()) << popcendl;	
-	return data;
+  while(popfilebuffer_ref->get_size_input_data() < size){
+    cout << "Before lock: id = " << identifier << " data = "<< popfilebuffer_ref->get_size_input_data() << "Size" << size <<  popcendl;
+    sem_wait(pt_read_locker);
+  }	
+  POPString data;
+  if(size != -1)
+    data = popfilebuffer_ref->buffer_get(size).c_str();
+  else 
+    data = popfilebuffer_ref->buffer_get_offset().c_str();
+  cout << "[POPFILEREADER] Sent data size " << strlen(data.GetString()) << popcendl;	
+  return data;
 }
 
 /**
@@ -97,8 +95,8 @@ POPString POPFileReader::read_current_buffer(long size)
  */
 void POPFileReader::set_offset(long offset)
 {
-	pfr_offset = offset;
-	popfilebuffer_ref->set_capacity(pfr_offset);
+  pfr_offset = offset;
+  popfilebuffer_ref->set_capacity(pfr_offset);
 }
    
 /**
@@ -108,8 +106,8 @@ void POPFileReader::set_offset(long offset)
  */
 void POPFileReader::set_pfm_accesspoint(paroc_accesspoint ap)
 {
-	pfm_ap = ap;
-	pfm_ref = new POPFileManager(pfm_ap);
+  pfm_ap = ap;
+  pfm_ref = new POPFileManager(pfm_ap);
 }
    
 /**
@@ -118,7 +116,7 @@ void POPFileReader::set_pfm_accesspoint(paroc_accesspoint ap)
  */
 paroc_accesspoint POPFileReader::get_pfm_accesspoint()
 {
-	return pfm_ap;
+  return pfm_ap;
 }
 
 /** 
@@ -128,7 +126,7 @@ paroc_accesspoint POPFileReader::get_pfm_accesspoint()
  */
 void POPFileReader::set_strip_path(POPString path)
 {
-	strip_path = path;
+  strip_path = path;
 }
    
 /**
@@ -137,7 +135,7 @@ void POPFileReader::set_strip_path(POPString path)
  */
 POPString POPFileReader::get_strip_path()
 {
-	return strip_path;
+  return strip_path;
 }
 
 //Specific POP-C++ annotation
